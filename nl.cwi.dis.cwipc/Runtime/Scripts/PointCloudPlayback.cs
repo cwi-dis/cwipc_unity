94a9bf8d3e34cec980f334494b7efceb939a86ea
Jack Jansen <Jack.Jansen@cwi.nl>
2023-02-24 17:25:31 +0100

    Getting started on script and prefab to do playback in a more orderly fashion.

---
+++ nl.cwi.dis.cwipc/Runtime/Scripts/PointCloudPlayback.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Cwipc
+{
+    /// <summary>
+    /// Play a sequence of prerecorded pointclouds (think: volumetric video)
+    /// </summary>
+    public class PointCloudPlayback : MonoBehaviour
+    {
+        public PrerecordedPointCloudReader pc_reader;
+        public PointCloudRenderer pc_renderer;
+        public bool playOnStart = false;
+        public bool preload = false;
+        public int loopCount = 0;
+        public string dirName = "";
+        public UnityEvent started;
+        public UnityEvent finished;
+
+        public string Name()
+        {
+            return $"{GetType().Name}";
+        }
+
+        private void Awake()
+        {
+            if (pc_reader == null)
+            {
+                pc_reader = GetComponentInChildren<PrerecordedPointCloudReader>();
+            }
+            if (pc_reader == null)
+            {
+                Debug.LogError($"{Name()}: no pc_reader found");
+            }
+            if (pc_renderer == null)
+            {
+                pc_renderer = GetComponentInChildren<PointCloudRenderer>();
+            }
+            if (pc_renderer == null)
+            {
+                Debug.LogError($"{Name()}: no pc_renderer found");
+            }
+            pc_reader.gameObject.SetActive(false);
+            pc_renderer.gameObject.SetActive(false);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            if (playOnStart)
+            {
+                Play(dirName);
+            }
+        }
+
+        public void Play(string _dirName)
+        {
+            dirName = _dirName;
+            pc_reader.dirName = dirName;
+            pc_reader.gameObject.SetActive(true);
+            pc_renderer.gameObject.SetActive(true);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
