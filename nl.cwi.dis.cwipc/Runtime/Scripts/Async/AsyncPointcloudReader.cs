6b2c04ee8efe0633aab431344bc1fb2e67eb00cc
BEELZEBUB\jack <Jack.Jansen@cwi.nl>
2026-02-17 17:13:40 +0100

    Stop the capturer.

--- nl.cwi.dis.cwipc/Runtime/Scripts/Async/AsyncPointcloudReader.cs
+++ nl.cwi.dis.cwipc/Runtime/Scripts/Async/AsyncPointcloudReader.cs
@@ -79,6 +79,9 @@ namespace Cwipc
         public override void Stop()
         {
             base.Stop();
+            reader.stop();
+            reader.free();
+            reader = null;
             if (outQueue != null && !outQueue.IsClosed()) outQueue.Close();
             if (out2Queue != null && !out2Queue.IsClosed()) out2Queue.Close();
             lock (this)
